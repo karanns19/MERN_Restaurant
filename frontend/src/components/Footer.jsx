03f8e73a9915c1ace4b2b7ec6f602677e1ad28b1
karanns19 <karanns.aero19@gmail.com>
2024-01-16 21:08:42 +0530

    changes commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,16 +7,16 @@ const Footer = () => {
         <div className="banner">
           <div className="left">ZEESHU</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
+            <p>Thane, Mumbai</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By Karan Singh</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By CodeWithZeeshu.</p>
+            <p>All Rights Reserved By Karan Singh.</p>
           </div>
         </div>
       </div>
